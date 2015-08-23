6f9d71b440c5bc0fa22e33b266837c5db8f746e6
José <jrstylletecnologia@hotmail.com>
2015-08-23 00:26:03 -0300

    Print one value in screen with commands echo and print

---
+++ PHP/php_basic/examples/3_printValue.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Print one value in screen with commands "echo" and "print"</title>
+</head>
+<body>
+    <?php
+    $name = "José Rodriguês";
+    $age = 20;
+    $br = "<br>";
+    #using the command "echo"
+    echo $name.$br.$age." Anos".$br;
+    #using command print
+    print $name.$br.$age." Anos".$br;
+?>
+</body>
+</html>
