1371888225a588d0943c0ad48eb6b99a29e61ba2
José <jrstylletecnologia@hotmail.com>
2015-08-24 03:53:02 -0300

    Created archive to work with operators in php

---
+++ PHP/php_basic/examples/4_operators.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Working with operators in php</title>
+</head>
+<body>
+    <?php
+    $x = 10; # 10 input value in variable x
+    $y = 10; # 10 input value in variable y
+    $br = "<br>"; #input value "<br>" tag to break row in screen
+    #Sum between two number
+    echo $x+$y;
+    echo $br;
+    #Division betwen two number
+    echo $x-$y;
+    echo $br;
+    ?>
+</body>
+</html>
