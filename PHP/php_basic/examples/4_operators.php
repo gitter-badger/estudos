63018c65d3a0df27677f49dd41bc4fdd35b57a9a
José <jrstylletecnologia@hotmail.com>
2015-08-25 01:45:10 -0300

    More operations added in php

--- PHP/php_basic/examples/4_operators.php
+++ PHP/php_basic/examples/4_operators.php
@@ -9,12 +9,19 @@
     $x = 10; # 10 input value in variable x
     $y = 10; # 10 input value in variable y
     $br = "<br>"; #input value "<br>" tag to break row in screen
-    #Sum between two number
+    #Sum between two numbers
     echo $x+$y;
     echo $br;
-    #Division betwen two number
+    #Division betwen two numbers
     echo $x-$y;
     echo $br;
+    #Multiplication between two numbers
+    echo 30 *20;
+    echo $br;
+    #Subtruction between two numbers
+    echo 20 - 10;
+    #operations with parentheses
+    echo (((10*2)+10)/2)-10;
     ?>
 </body>
 </html>
