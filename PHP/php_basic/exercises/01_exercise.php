af69a5deec17d3f5c4b22aebe7daefdb5d38040c
José <jrstylletecnologia@hotmail.com>
2015-08-25 23:24:47 -0300

    Solved exercises

---
+++ PHP/php_basic/exercises/01_exercise.php
@@ -0,0 +1,53 @@
+<?php
+    # Exercise 1
+    $br = "<br>";
+    $a = 3+4*5;
+    echo $a;
+    echo $br;
+    # Exercise 2
+    $br = "<br>";
+    $a = 1+12/((7+2)/3+(6-2));
+    echo $a;
+    echo $br;
+    # Exercise 3
+    $br = "<br>";
+    $a = sqrt(9) + sqrt(16);
+    echo $a;
+    echo $br;
+    # Exercise 4
+    $br = "<br>";
+    $a = 8/4+3*2;
+    echo $a;
+    echo $br;
+    # Exercise 5
+    $br = "<br>";
+    $a = 3+16/2+5;
+    echo $a;
+    echo $br;
+    # Exercise 6
+    $br = "<br>";
+    $a = 21/4/2;
+    echo $a;
+    echo $br;
+    # Exercise 7
+    $br = "<br>";
+    $a = 2*(10-3*3)-1;
+    echo $a;
+    echo $br;
+    # Exercise 8
+    $br = "<br>";
+    $a = 24 / 4-2;
+    echo $a;
+    echo $br;
+    # Exercise 9
+    $br = "<br>";
+    $a = 5*(3+(2+3))/2+1;
+    echo $a;
+    echo $br;
+    # Exercise 10
+    $br = "<br>";
+    $a = 11 % 4+9 / 3;
+    echo $a;
+    echo $br;
+    
+?>
