f0b344ead0e6175e6c9388c883d0f1359ee5d2fe
José <jrstylletecnologia@hotmail.com>
2015-08-26 01:58:08 -0300

    created empresa.php page

---
+++ PHP/php_basic/exercises/03_exercise/empresa.php
@@ -0,0 +1,8 @@
+<?php include_once("header.php");?>
+    <section>
+        <article>
+            <h1>Título: <?= $title; ?></h1>
+            <p>Conteúdo de teste para a página: <?= $title; ?></p>
+        </article>
+    </section>
+<?php include_once("footer.php");?>
