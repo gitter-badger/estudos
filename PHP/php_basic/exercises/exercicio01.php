577f24b79b2f8f65a494386de5aa6d858020eeb5
José Rodrigues <jrstylletecnologia@hotmail.com>
2015-08-30 00:52:41 -0300

    Update and rename 01_exercise.php to exercicio01.php

--- PHP/php_basic/exercises/exercicio01.php
+++ PHP/php_basic/exercises/exercicio01.php
@@ -1,50 +1,50 @@
 <?php
-    # Exercise 1
+    # Exercício 1
     $br = "<br>";
     $a = 3+4*5;
     echo $a;
     echo $br;
-    # Exercise 2
+    # Exercício 2
     $br = "<br>";
     $a = 1+12/((7+2)/3+(6-2));
     echo $a;
     echo $br;
-    # Exercise 3
+    # Exercício 3
     $br = "<br>";
     $a = sqrt(9) + sqrt(16);
     echo $a;
     echo $br;
-    # Exercise 4
+    # Exercício 4
     $br = "<br>";
     $a = 8/4+3*2;
     echo $a;
     echo $br;
-    # Exercise 5
+    # Exercício 5
     $br = "<br>";
     $a = 3+16/2+5;
     echo $a;
     echo $br;
-    # Exercise 6
+    # Exercício 6
     $br = "<br>";
     $a = 21/4/2;
     echo $a;
     echo $br;
-    # Exercise 7
+    # Exercício 7
     $br = "<br>";
     $a = 2*(10-3*3)-1;
     echo $a;
     echo $br;
-    # Exercise 8
+    # Exercício 8
     $br = "<br>";
     $a = 24 / 4-2;
     echo $a;
     echo $br;
-    # Exercise 9
+    # Exercício 9
     $br = "<br>";
     $a = 5*(3+(2+3))/2+1;
     echo $a;
     echo $br;
-    # Exercise 10
+    # Exercício 10
     $br = "<br>";
     $a = 11 % 4+9 / 3;
     echo $a;
